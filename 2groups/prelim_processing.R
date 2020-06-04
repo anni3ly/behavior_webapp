171626c4454ac257c0ccd6defe8768bec1c8df7e
Annie Ly <anni3.ly@gmail.com>
2020-06-04 19:11:54 -0400

    Add files via upload

---
+++ 2groups/prelim_processing.R
@@ -0,0 +1,79 @@
+library(plyr)
+library(lubridate)
+library(DT)
+library(lme4)
+library(car)
+library(stringr)
+library(readr)
+library(xlsx)
+library(data.table)
+library(ggplot2)
+library(shiny)
+library(forcats)
+library(purrr)
+library(tidyr)
+library(emmeans)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(shinycssloaders)
+library(shinyjs)
+library(dplyr) #ALWAYS LOAD DPLYR LAST; OTHERWISE IT WILL MASK FUNCTIONS FROM OTHER PACKAGES
+
+data_summary <- function(data, varname, groupnames){
+  summary_func <- function(x, col){
+    c(mean = mean(x[[col]], na.rm=TRUE),
+      sd = sd(x[[col]], na.rm=TRUE),
+      se = sd(x[[col]], na.rm=TRUE)/sqrt(length(x[[col]])))
+  }
+  data_sum<-ddply(data, groupnames, .fun=summary_func,
+                  varname)
+  data_sum <- plyr::rename(data_sum, c(varname="mean"))
+  return(data_sum)
+}
+
+session_count<-function(df) {
+  seq(1, nrow(df), by=1)
+}
+
+
+FR_data <- read.csv(file="FR_data.csv")
+FR_data_raw <- read.csv(file="FR_data.csv")
+FR_data$fr<- as.character(FR_data$fr)
+
+FR_data$fr<- factor(FR_data$fr, levels=c("1","5")) #factoring fr
+
+FR_data <- plyr::mutate(FR_data, activity=beam1+beam2+beam3+beam4) # adding locomotor activity
+FR_data <- mutate(FR_data, percent.active=active/(active+inactive))
+FR_data <- mutate(FR_data, percent.inactive=inactive/(active+inactive))
+
+FR_data$edate<-mdy(FR_data$edate)
+
+FR_data$subject<-fct_inorder(FR_data$subject)
+FR_data<- as.data.table(FR_data)
+FR_data<- FR_data[order(FR_data$edate),]
+FR_data<-FR_data %>% group_by(subject)%>% nest()
+FR_data<-FR_data %>% mutate(session=map(data,session_count))
+FR_data<-FR_data %>% unnest()
+FR_data<-with(FR_data, FR_data[order(session),])
+
+# TRIAL AND TIMEOUT ACTIVES
+
+for (q in (1:nrow(FR_data))) ({
+  if (FR_data$fr[q]==1) ({
+    FR_data$trial.active[q]=FR_data$reward[q]
+    FR_data$timeout.active[q]=FR_data$active[q]-FR_data$trial.active[q]
+  }) 
+  if (FR_data$fr[q]==5) ({
+    FR_data$trial.active[q]=FR_data$reward[q]*5
+    FR_data$timeout.active[q]=FR_data$active[q]-FR_data$trial.active[q]
+  })
+  if (FR_data$fr[q]=="pr") ({
+    FR_data$trial.active[q]=FR_data$active[q]-FR_data$timeout.active[q]
+    FR_data$timeout.active[q]=FR_data$timeout.active[q]
+  })
+  
+})
+
+
+options(contrasts=c("contr.sum", "contr.poly"))
+appCSS <- "#loading-content {position: absolute; background: #000000; opacity: 0.8; z-index: 100; left: 0; right: 0; height: 100%; text-align: center; color: #FFFFFF;}"
---
+++ 2groups/server.R
@@ -0,0 +1,335 @@
+server = function(input, output)({
+  group_1 = c("saline")
+  group_2 = c("heroin")
+  
+  #----------------------------------------
+  # Reactive data sets ---- 
+  reactive.data1 <- reactive({
+    #data_summary(FR_data, varname=input$y, groupnames=c("session", "group", "fr", "sex"))
+    data_summary(FR_data, varname=input$y, groupnames=c("session", "group", "fr"))
+  })
+  
+  reactive.data2<- reactive({
+    subset(reactive.data1(),
+           session >= input$session[1] & session <=input$session[2])
+  })
+  
+  reactive.data3<- reactive({
+    subset(FR_data, group==group_1)
+  })
+  reactive.data4<- reactive({
+    subset(FR_data, group==group_2)
+  })
+  
+  reactive.data6<- reactive({
+    select(FR_data, subject, session, group, boxnum, fr, active, inactive, reward) %>% subset(group==group_1)
+  })
+  reactive.data7<- reactive({
+    select(FR_data, subject, session, group, boxnum, fr, active, inactive, reward) %>% subset(group==group_2)
+  })
+  
+  reactive.data9<- reactive({
+    keep4    <- subset(FR_data, group==group_1)[ vals4$keeprows4, , drop = FALSE]
+    keep5    <- subset(FR_data, group==group_2)[ vals5$keeprows5, , drop = FALSE]
+    exclude_final<-rbind(keep4,keep5)
+    level1_9<-tidyr::gather(exclude_final, lever, presses, c(active, inactive)) 
+    level1_9<-with(level1_9, level1_9[order(session, subject),])
+  })
+  
+  reactive.data10<- reactive({
+    lmer(presses ~ session*lever*group + (group|subject), data=reactive.data9())
+  })
+  
+  reactive.data13<- reactive({
+    keep4    <- subset(FR_data, group==group_1)[ vals4$keeprows4, , drop = FALSE]
+    keep5    <- subset(FR_data, group==group_2)[ vals5$keeprows5, , drop = FALSE]
+    exclude_final<-rbind(keep4,keep5)
+    level1_9<-tidyr::gather(exclude_final, lever, presses, c(active, inactive)) 
+    level1_9<-with(level1_9, level1_9[order(session, subject),])
+    level1_9$session<- factor(level1_9$session)
+    return(level1_9)
+  })
+  
+  reactive.data14<- reactive({
+    lmer(presses ~ session*lever*group + (group|subject), data=reactive.data13())
+  })
+  
+  reactive.data16<- reactive({
+    keep4    <- subset(FR_data, group==group_1)[ vals4$keeprows4, , drop = FALSE]
+    keep5    <- subset(FR_data, group==group_2)[ vals5$keeprows5, , drop = FALSE]
+    exclude_final<-rbind(keep4,keep5)
+    level1_9<-tidyr::gather(exclude_final, lever, presses, c(active, inactive)) 
+    level1_9<-with(level1_9, level1_9[order(session, subject),])
+    data15_1<-unite(level1_9, session, lever, col="session_lever", sep="_")
+    data15_1<-dplyr::select(data15_1, subject, group, session_lever, presses)
+    data15_1$session_lever<-data15_1$session_lever %>% factor() %>% fct_inorder()
+    data16_1<-spread(data15_1, session_lever, presses)
+    group<- data16_1$group
+    subject<- data16_1$subject
+    data16_1$group<-NULL
+    data16_1$subject<-NULL
+    data16_1<-t(apply(data16_1, 1, function(x) c(x[is.na(x)], x[!is.na(x)])))
+    data16_1<-data16_1[,apply(data16_1, 2, function(x) !any(is.na(x)))]
+    data16_1<-data16_1[,(ncol(data16_1)-5):ncol(data16_1)]
+    colnames(data16_1)<-c("x_active", "x_inactive", "y_active", "y_inactive", "z_active", "z_inactive")
+    data16_1<-as.data.frame(data16_1)
+    data16_1<- cbind(subject, group, data16_1)
+    data16_1<- gather(data16_1, "session_lever", "presses", x_active:z_inactive)
+    data16_1<- separate(data16_1, session_lever, c("session", "lever"), sep="_")
+    data16_1$presses<- as.numeric(data16_1$presses)
+    return(data16_1)
+  })
+  
+  reactive.data12<- reactive({
+    lmer(presses ~ session*lever*group + (group|subject), data=reactive.data16())
+  })
+  
+  vals4 <- reactiveValues(
+    keeprows4 = rep(TRUE, nrow(subset(FR_data, group==group_1)))
+  )
+  
+  vals5 <- reactiveValues(
+    keeprows5 = rep(TRUE, nrow(subset(FR_data, group==group_2)))
+  )
+  
+  download.plot<- reactive({
+    ggplot(reactive.data2(), aes(x=session, y= mean, group = interaction(group), color=interaction(group))) + 
+      geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=position_dodge(0.05)) +
+      geom_point(aes(shape=fr), size = 5) +
+      geom_line(aes(linetype=interaction(group)), size=1) + 
+      ggtitle(input$title) +
+      ylab(input$y) +
+      theme_classic()
+  })
+  
+  #----------------------------------------     
+  # Plots 
+  
+  # Main plots ----
+  output$plot<- renderPlot({
+    ggplot(reactive.data2(), aes(x=session, y= mean, group = interaction(group), color=interaction(group))) + 
+      geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.05, size=1) +
+      geom_point(aes(shape=fr), size = 5) +
+      geom_line(aes(linetype=interaction(group)), size=2) +
+      ggtitle(input$title) +
+      ylab(input$y) +
+      scale_shape_discrete(name="FR Ratio Schedule") +
+      theme_classic() +
+      theme(axis.title.x = element_text(size=20, face="bold"),
+            axis.title.y = element_text(size=20, face="bold"), 
+            axis.text = element_text(size = 20, face="bold"),
+            axis.line = element_line(size = 1)) 
+  })
+  
+  output$plot2<- renderPlot({
+    ggplot(reactive.data3(), aes_string(y=input$y, x="session", group = "group", color="group")) +
+      geom_line() +
+      geom_point(aes(shape=fr), size=3) + 
+      facet_wrap(~ subject) +
+      scale_shape_discrete(name="FR Ratio Schedule") +
+      theme_bw() +
+      theme(axis.title.x = element_text(size=20, face="bold"),
+            axis.title.y = element_text(size=20, face="bold"), 
+            axis.text = element_text(size = 20, face="bold"),
+            axis.line = element_line(size = 1)) 
+  }, height=1000)
+  
+  output$plot3<- renderPlot({
+    ggplot(reactive.data4(), aes_string(y=input$y, x="session", group = "group", color="group")) +
+      geom_line() +
+      geom_point(aes(shape=fr), size=3) + 
+      facet_wrap(~ subject) +
+      scale_shape_discrete(name="FR Ratio Schedule") +
+      theme_bw() +
+      theme(axis.title.x = element_text(size=20, face="bold"),
+            axis.title.y = element_text(size=20, face="bold"), 
+            axis.text = element_text(size = 20, face="bold"),
+            axis.line = element_line(size = 1)) 
+  }, height=1000)
+  
+  output$plot4<- renderPlot({
+    keep4    <- subset(FR_data, group==group_1)[ vals4$keeprows4, , drop = FALSE]
+    exclude4 <- subset(FR_data, group==group_1)[!vals4$keeprows4, , drop = FALSE]
+    ggplot(keep4, aes_string(y=input$y, x="session")) +
+      geom_boxplot(aes(fill=factor(session))) +
+      geom_point() +
+      labs(y=input$y, x="session") +
+      theme_bw() +
+      theme(axis.title.x = element_text(size=20, face="bold"),
+            axis.title.y = element_text(size=20, face="bold"), 
+            axis.text = element_text(size = 20, face="bold"),
+            axis.line = element_line(size = 1)) 
+  })
+  
+  output$plot5<- renderPlot({
+    keep5    <- subset(FR_data, group==group_2)[ vals5$keeprows5, , drop = FALSE]
+    exclude5 <- subset(FR_data, group==group_2)[!vals5$keeprows5, , drop = FALSE]
+    ggplot(keep5, aes_string(y=input$y, x="session")) +  
+      geom_boxplot(aes(fill=factor(session))) +
+      geom_point() +
+      labs(y=input$y, x="session") +
+      theme_bw() +
+      theme(axis.title.x = element_text(size=20, face="bold"),
+            axis.title.y = element_text(size=20, face="bold"), 
+            axis.text = element_text(size = 20, face="bold"),
+            axis.line = element_line(size = 1)) 
+  })
+  
+  output$int_plot<- renderPlot({
+    emmip(reactive.data14(), group ~ session | lever)
+  })
+  
+  output$int_plot2<- renderPlot({
+    emmip(reactive.data12(), group ~ session | lever)
+  })
+  
+  output$residualplot3<- renderPlot({
+    qqnorm(residuals(reactive.data12()))
+    qqline(residuals(reactive.data12()))
+  })
+  
+  output$residualplot4<- renderPlot({
+    plot(reactive.data12(), resid(., scaled=TRUE) ~ presses | group)
+  })
+  
+  output$residualplot1<- renderPlot({
+    qqnorm(residuals(reactive.data10()))
+    qqline(residuals(reactive.data10()))
+  })
+  
+  output$residualplot2<- renderPlot({
+    plot(reactive.data10(), resid(., scaled=TRUE) ~ presses | group)
+  })
+  
+  #---------------------------------------- 
+  # Render Prints ----
+  
+  output$hover_info1<- renderPrint({
+    nearPoints(reactive.data6(), input$plot_hover1, xvar = "session", yvar = input$y)
+  })
+  
+  output$hover_info2<- renderPrint({
+    nearPoints(reactive.data7(), input$plot_hover2, xvar = "session", yvar = input$y)
+  })
+  
+  
+  output$stats_wald2<-renderPrint({
+    Anova(lmer(presses ~ session*lever*group + (group|subject), data=reactive.data9()), type=2)
+  })
+  
+  output$stats_wald3<-renderPrint({
+    Anova(lmer(presses ~ session*lever*group + (group|subject), data=reactive.data9()), type=3)
+  })
+  
+  output$stats_tukey<-renderPrint({
+    TukeyHSD(x=aov(presses ~ group + lever, data=reactive.data9()), conf.level=0.95)
+  })
+  
+  
+  output$last3sessions_anova3<-renderPrint({
+    Anova(lmer(presses ~ session*lever*group + (group|subject), data=reactive.data16()), type=3)
+    #aov(presses~group*lever*session + Error(subject/session), data=reactive.data16())
+  })
+  
+  output$last3sessions_anova2<-renderPrint({
+    Anova(lmer(presses ~ session*lever*group + (group|subject), data=reactive.data16()), type=2)
+    #aov(presses~group*lever*session + Error(subject/session), data=reactive.data16())
+  })
+  
+  output$stats_tukey2<-renderPrint({
+    TukeyHSD(x=aov(presses ~ group + lever, data=reactive.data16()), conf.level=0.95)
+  })
+  
+  output$contrasts1<- renderPrint({
+    emmeans(reactive.data10(), pairwise ~ lever + group)
+  })
+  
+  output$contrasts2<- renderPrint({
+    emmeans(reactive.data12(), pairwise ~ lever + group)
+  })
+  
+  #----------------------------------------           
+  # Render Tables ----
+  
+  output$master.data<- DT::renderDataTable({
+    datatable(FR_data, filter='top', options=list(pagelength=25), class = 'cell-border stripe')
+  })
+  
+  output$table<- DT::renderDataTable({
+    datatable(reactive.data2(), filter='top', options=list(pagelength=25), class = 'cell-border stripe')
+  })            
+  
+  #---------------------------------------- 
+  # Download Handlers ----
+  output$downloadplot<- downloadHandler(
+    filename = function() { paste(input$y, '.png', sep = '')},
+    content = function(file) {
+      ggsave(file, plot=download.plot(), device="png")
+    })
+  
+  output$downloaddata<- downloadHandler(
+    filename = function(){ paste0("data.", input$filetype, sep = '')},
+    content = function(file){
+      if(input$filetype == "csv"){ 
+        write_csv(FR_data_raw, file) 
+      }
+      if(input$filetype == "tsv"){ 
+        write_tsv(FR_data_raw, file) 
+      }
+      if(input$filetype =="xlsx"){
+        #write_excel_csv(FR_data, file)
+        write.xlsx(FR_data_raw, file)
+      }
+    })
+  
+  output$downloaddata2<- downloadHandler(
+    filename = function(){ paste0("data.", input$filetype, sep = '')},
+    content = function(file){
+      if(input$filetype == "csv"){ 
+        write_csv(FR_data, file) 
+      }
+      if(input$filetype == "tsv"){ 
+        write_tsv(FR_data, file) 
+      }
+      if(input$filetype =="xlsx"){
+        #write_excel_csv(FR_data, file)
+        write.xlsx(FR_data, file)
+      }
+    })
+  
+  
+  
+  #---------------------------------------- 
+  # Toggle points that are clicked ----
+  
+  observeEvent(input$plot_click1, {
+    res4 <- nearPoints(subset(FR_data, group==group_1), input$plot_click1, allRows = TRUE)
+    vals4$keeprows4 <- xor(vals4$keeprows4, res4$selected_)
+    
+    res5 <- nearPoints(subset(FR_data, group==group_2), input$plot_click2, allRows = TRUE)
+    vals5$keeprows5 <- xor(vals5$keeprows5, res5$selected_)
+  })
+  
+  
+  #---------------------------------------- 
+  # Toggle points that are brushed, when button is clicked ----
+  observeEvent(input$exclude_toggle, {
+    res4 <- brushedPoints(subset(FR_data, group==group_1), input$plot_brush1, allRows = TRUE)
+    vals4$keeprows4 <- xor(vals4$keeprows4, res4$selected_)
+    
+    res5 <- brushedPoints(subset(FR_data, group==group_2), input$plot_brush2, allRows = TRUE)
+    vals5$keeprows5 <- xor(vals5$keeprows5, res5$selected_)
+  })
+  
+  #----------------------------------------      
+  # Reset all toggle points ----
+  observeEvent(input$exclude_reset, {
+    vals4$keeprows4 <- rep(TRUE, nrow(subset(FR_data, group==group_1)))
+    vals5$keeprows5 <- rep(TRUE, nrow(subset(FR_data, group==group_2)))
+  })
+  
+  hide(id = "loading-content", anim = TRUE, animType = "fade")
+  
+})
+
+shinyApp(ui = ui,server = server)
